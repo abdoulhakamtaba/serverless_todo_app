fa55021d05353be744c89c632f9158a75654782d
abdoulhakamtaba <abdoulhakamtaba@gmail.com>
2022-10-11 13:51:38 +0100

    Made some changes

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,22 +1,25 @@
 import 'source-map-support/register'
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-import * as middy from 'middy'
-import { cors } from 'middy/middlewares'
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
-import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
-import { getUserId } from '../utils';
+import { getAllToDo } from '../../helpers/todos'
 
-// TODO: Get all TODO items for a current user
-export const handler = middy(
-  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    // Write your code here
-    const todos = '...'
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  // TODO: Get all TODO items for a current user
+  console.log("Processing Event ", event);
+  const authorization = event.headers.Authorization;
+  const split = authorization.split(' ');
+  const jwtToken = split[1];
 
-    return undefined
+  const toDos = await getAllToDo(jwtToken);
 
-handler.use(
-  cors({
-    credentials: true
-  })
-)
+  return {
+      statusCode: 200,
+      headers: {
+          "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+          "items": toDos,
+      }),
+  }
+};
